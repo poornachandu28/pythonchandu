7e993b66d738b277ef701d7d1673228656175949
poornachandu28 <110621498+poornachandu28@users.noreply.github.com>
2022-10-11 16:17:38 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-employee={"name":"tim",
-          "birthday": "1990-03-10",
-          'age': 30,
-          "job": "DevOps Engineer"
-}
-employee.__delitem__('age')
-employee['job'] = 'Software Engineer'
-for key, value in employee.items():
-    print(key,':',value)
