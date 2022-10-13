2d6680beb6b4055cefad94989123ab52012390ef
poornachandu28 <110621498+poornachandu28@users.noreply.github.com>
2022-10-13 18:13:34 +0530

    outputs for mymodule

---
+++ main(output for module).py
@@ -0,0 +1,23 @@
+import mymodule
+
+
+
+
+####### output #########
+
+
+
+
+name of youngest employee: chandu
+age of youngest employee: 23
+2
+4
+6
+8
+10
+Enter a string: CoMPAny
+upper: 4
+lower: 3
+
+Process finished with exit code 0
+
