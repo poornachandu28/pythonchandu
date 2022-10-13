487030ce6ea88dffb361884502b99942c35b9925
poornachandu28 <110621498+poornachandu28@users.noreply.github.com>
2022-10-13 14:52:41 +0530

    Update and rename python.py to python(5).py

--- python(5).py
+++ python(5).py
@@ -37,3 +37,41 @@ while loop == 1:
             print("%d is not valid input. Please enter the correct option." % choice)
     except ValueError and TypeError:
         print("%r is not valid input.  Please enter 1, 2, 3, 4." % choice)
+        
+        
+        
+        
+        
+        
+        
+        #######  output  #####
+        
+        
+       
+    
+ Hey user your options are Here, please choose anyone:
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+choose your option(1,2,3,4): 1
+Enter the First Number: 2
+Enter the Second Number: 4
+2 + 4 = 6
+ Hey user your options are Here, please choose anyone:
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+choose your option(1,2,3,4): 2
+Enter the First Number: 5
+Enter the Second Number:3
+5 - 3 = 2
+ Hey user your options are Here, please choose anyone:
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+choose your option(1,2,3,4): exit
+The number of calculations user did: 2
+continue (yes/no): no
